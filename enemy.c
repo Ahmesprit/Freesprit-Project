7b9f2da83a39bce91bda3ccbbb9ff7b0fe0d6772
azizallouche89 <48280130+azizallouche89@users.noreply.github.com>
2019-03-16 11:37:34 +0100

    Update enemy.c

--- enemy.c
+++ enemy.c
@@ -23,3 +23,42 @@ enm.posEnm.w = enm.e->w;
  SDL_BlitSurface(enm.e, NULL, screen, &enm.posEnm);
  SDL_Flip (screen);
 }
+
+void UpdateEnnemi(STATE S,SDL_Rect *positionennemi, int dir)
+{
+	int i;
+   switch (S)
+   {
+     case WAITING :
+              //AnnimateEnnemi(E,0,e);
+              break;
+     case HAREB : 
+                  //AnnimateEnnemi(E,0,e);
+                   if (dir==2)
+                   for (i=0; i<5; i++)
+                   {
+                    positionennemi->x = positionennemi->x - 3;
+                    positionennemi->y +=(-1/9)*positionennemi->x+2;
+                   }
+                   if (dir==1)
+                   for (i=0; i<5; i++)
+                   {
+                    positionennemi->x += 3;
+                    positionennemi->y -=(-1/9)*positionennemi->x+2;
+                   }
+                   if (dir==4)
+                   for (i=0; i<5; i++)
+                   {
+                    positionennemi->x += 3;
+                    positionennemi->y +=(-1/9)*positionennemi->x+2;
+                   }
+                   if (dir==3)
+                   for (i=0; i<5; i++)
+                   {
+                    positionennemi->x -= 3;
+                    positionennemi->y -=(-1/9)*positionennemi->x+2;
+                   }
+                  break;
+   }
+
+}
