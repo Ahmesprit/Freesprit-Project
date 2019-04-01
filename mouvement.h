fb3a4d56b62225e45b464bcc7c3dc72f081eb9f3
Ahmed Esprit <47778846+Ahmesprit@users.noreply.github.com>
2019-04-01 06:27:05 +0100

    unnecessary file

--- mouvement.h
+++
@@ -1,6 +0,0 @@
-#ifndef MOUVEMENT_H_INCLUDED
-#define MOUVEMENT_H_INCLUDED
-
-void moveKeyboard (SDL_Surface *screen, SDL_Event event, SDL_Rect * posobj, SDL_Surface * obj, SDL_Surface *back, SDL_Rect *posback, SDL_Rect *camera);
-void scrolling (SDL_Surface *back, SDL_Rect * camera, SDL_Event, SDL_Surface *screen);
-#endif
